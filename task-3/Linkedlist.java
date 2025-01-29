4c9b2bc6bdd0cdeb0ad48add9c74f8a6b4d7ed70
Tring-Kishore <kishore.c@tringapps.com>
2025-01-29 15:26:43 +0530

    Fix : Adding file of Insertion at Any position and Searching

--- task-3/Linkedlist.java
+++ task-3/Linkedlist.java
@@ -39,6 +39,11 @@ public class Linkedlist {
         System.out.println("\nAfter Insert at Last");
         Node head3 = insertNodeLast(0, head2);
         PrintLinkedList(head3);
+        System.out.println("\nAfter Insert at Any Position");
+        Node head4 = insertKPosition(6, 3, head3);
+        PrintLinkedList(head4);
+        System.out.println();
+        searchingLL(5, head4);
     }
     // Linked List Creation
     public static Node constructLL(int[] arr)
@@ -96,5 +101,61 @@ public class Linkedlist {
         }
         mover.next = newnode;
         return head;
+    }
+    public static Node insertKPosition(int element,int index,Node head)
+    {
+        int flag = 0;
+        if(head == null)
+        {
+            return new Node(element);
+        }
+        Node newnode = new Node(element);
+        if(index == 0)
+        {
+            Node newnNode = insertNodeFirst(element, head);
+            return newnNode;
+        }
+        int i = 0;
+        Node mover = head;
+        Node prev = null;
+        while(mover != null)
+        {
+            if(i == index)
+            {
+                prev.next = newnode;
+                newnode.next = mover;
+                flag = 1;
+                return head;
+            }
+            prev = mover;
+            mover = mover.next;
+            i++;
+        }
+        if(flag == 0)
+        {
+            Node newnNode = insertNodeLast(element, head);
+        }
+        return head;
+    }
+    public static void searchingLL(int element,Node head)
+    {
+        if(head == null)
+        {
+            System.out.println("Element Not Found");
+            System.exit(0);
+        }
+        Node mover = head;
+        int i = 0;
+        while(mover != null)
+        {
+            if(mover.data == element)
+            {
+                System.out.println("The element "+element+" it was in index of "+i);
+                System.exit(0);
+            }
+            i++;
+            mover = mover.next;
+        }
+        System.out.println("Element Not Found");
     }
 }
