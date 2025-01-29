0337819d5a3caab0f1ee199f6ced4a27301055f0
Tring-Kishore <kishore.c@tringapps.com>
2025-01-29 12:38:47 +0530

    Adding a Array File

---
+++ task-3/Arrays.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Arrays{
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a Number to Set Size Of the Array : ");
+        int n = s.nextInt();
+        // Declaring an Array
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            System.out.println("Enter the number : ");
+            arr[i] = s.nextInt();
+        }
+
+        //Traversing an array
+        System.out.println("The Array Elements are : ");
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
